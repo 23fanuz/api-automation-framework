180f630f708158731354f62ef3b6681da34a23c4
23fanuz <fanuzstephanie@gmail.com>
2026-04-20 14:29:58 -0400

    Refactor tests to use ProductAPI service layer

---
+++ services/product_api.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import APIRequestContext
+
+class ProductAPI:
+    def __int__(self, request_context: APIRequestContext):
+        self.request = request_context
+        self.base_url = "https://fakestoreapi.com"
+
+    def get_all_products(self):
+        response = self.request.get(f"{self.base_url}/products")
+        return response
--- tests/test_products.py
+++ tests/test_products.py
@@ -1,14 +1,17 @@
 import pytest
-from playwright.sync_api import  sync_playwright
+from playwright.sync_api import sync_playwright
+from services.product_api import ProductAPI
 
 def test_get_all_products():
     with sync_playwright() as p:
         request_context = p.request.new_context()
 
-        response = request_context.get("https://fakestoreapi.com/products")
+        product_api = ProductAPI(request_context)
+
+        response = product_api.get_all_products()
+
         assert response.status == 200
 
         data = response.json()
         assert isinstance(data, list)
-        assert len(data) > 0
 
