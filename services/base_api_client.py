4735100b56005265dff9cb6cbc4d272dc9527b65
23fanuz <fanuzstephanie@gmail.com>
2026-05-07 13:11:43 -0400

    add logging cabapilities to base_api_client

--- services/base_api_client.py
+++ services/base_api_client.py
@@ -1,13 +1,33 @@
+from utils.logger import get_logger
 class BaseAPIClient:
     def __init__(self, api_context, base_url: str):
         self.api_context = api_context
         self.base_url = base_url
+        self.logger = get_logger(self.__class__.__name__)
 
     def get(self, endpoint: str):
-        return self.api_context.get(f"{self.base_url}{endpoint}")
+        url = f"{self.base_url}{endpoint}"
+        self.logger.info(f"GET{url}")
+
+        response = self.api_context.get(url)
+        self.logger.info(f"Response status: {response.status}")
+        return response
+
 
     def post(self, endpoint: str, data=None):
-        return self.api_context.post(f"{self.base_url}{endpoint}", data=data)
+        url = f"{self.base_url}{endpoint}"
+        self.logger.info(f"POST {url}")
+
+        response = self.api_context.post(url, data=data)
+
+        self.logger.info(f"Response status: {response.status}")
+        return response
 
     def delete(self, endpoint: str):
-        return self.api_context.delete(f"{self.base_url}{endpoint}")
+        url = f"{self.base_url}{endpoint}"
+        self.logger.info(f"DELETE {url}")
+
+        response = self.api_context.delete(url)
+
+        self.logger.info(f"Response status: {response.status}")
+        return response
