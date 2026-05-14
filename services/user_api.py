5240c036e2715f706732ef3d764b1fa8da02c69f
23fanuz <fanuzstephanie@gmail.com>
2026-05-14 16:21:56 -0400

    add comments to user_api service

--- services/user_api.py
+++ services/user_api.py
@@ -2,12 +2,28 @@ from services.base_api_client import BaseAPIClient
 
 
 class UserAPI(BaseAPIClient):
+    """
+    UserAPI takes care of all user-related api operations.
 
+    This is a service layer abstraction that isolates endpoint login from
+    test logic
+    """
+
+    # Initialize base client with shared API context and base URL
     def __init__(self, api_context):
         super().__init__(api_context, "https://fakestoreapi.com")
 
     def get_all_users(self):
+        """
+        GET /users
+        :returns list of all users
+        """
         return self.get("/users")
 
     def create_user(self, payload):
+        """
+        POST /users
+        Creates a new user using the provided payload
+        Note: FakeStoreAPI only returns ID, not a full user object.
+        """
         return self.post("/users", data=payload)
