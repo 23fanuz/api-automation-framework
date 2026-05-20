16d21af97b1857394ef59c19c4ad493683284fbb
23fanuz <fanuzstephanie@gmail.com>
2026-05-19 22:32:36 -0400

    refactor product tests to use centralized validation layer

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,6 +1,6 @@
 from services.product_api import ProductAPI
-from models.product_model import Product
 import allure
+from utils.validation import  validate_response
 
 
 def test_get_all_products(api_context):
@@ -9,22 +9,11 @@ def test_get_all_products(api_context):
     with allure.step("Send GET request to /products"):
         response = product_api.get_all_products()
 
-    with allure.step("Attach raw response"):
-        allure.attach(
-            str(response.json()),
-            name="Response JSON",
-            attachment_type=allure.attachment_type.JSON
-        )
-
     with allure.step("Verify status code is 200"):
         assert response.status == 200
 
-    with allure.step("Parse response JSON"):
-        products = response.json()
-
     with allure.step("Validate response schema using Pydantic"):
-        validated_products = [Product(**product) for product in products]
+        products = validate_response("GET", "/products", response)
 
     with allure.step("Verify at least one product exists"):
-        assert len(validated_products) > 0
-
+        assert len(products) > 0
