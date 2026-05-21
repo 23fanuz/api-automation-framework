873cff038ea68dc48f17905871478373b84fb63e
23fanuz <fanuzstephanie@gmail.com>
2026-05-20 20:30:13 -0400

    refactor: introduce APIContractViolationError and dynami model parsing

--- utils/validation.py
+++ utils/validation.py
@@ -1,29 +1,34 @@
-from utils.schema_registry import SCHEMA_REGISTRY
+from pydantic import ValidationError
 
 
-def  validate_response(method: str, endpoint: str, response):
+class APIContractViolationError(AssertionError):
     """
-    Automatically validates API responses based on endpoint mappings + HTTP method
-
-    Creates a centralized contract validation system.
+    Custom exception for clean API contract violation reporting.
     """
+    pass
+
+
+def validate_payload(response, model):
+    """
+        Validates a raw response against a provided Pydantic model
+        """
 
     try:
         response_json = response.json()
+    except ValueError:
+        raise APIContractViolationError("Failed to parse response as JSON")
 
-        # Find expected schema
-        model = SCHEMA_REGISTRY.get((method, endpoint))
-
-        if not model:
-            raise ValueError(f"No schema registered for {method} {endpoint}")
-
+    try:
         if isinstance(response_json, list):
             return [model(**item) for item in response_json]
-
         return model(**response_json)
 
-    except Exception as e:
-        raise AssertionError(
-            f"Validation failed for endpoint {endpoint}: {str(e)}\n"
-            f"Response JSON: {response_json}"
-        )
+    except ValidationError as e:
+        # Catch Pydantic's specific error and format it
+        error_msg = f"Contract Violation for {model.__name__}:\n"
+        for error in e.errors():
+            # e.errors() -> a list of dictionaries with field locations and messagesa
+            field = " -> ".join([str(loc) for loc in error['loc']])
+            error_msg += f"- Field '{field}: {error['msg']}\n"
+
+        raise APIContractViolationError(error_msg)
